d1a57009d54d7097fc5b6a9a8ad50d4274b425ec
Mercandj <modjow.jm9@gmail.com>
2020-07-12 22:03:00 +0200

    [Script] Give PATH after build to have emulator command working

--- aosp_build.sh
+++ aosp_build.sh
@@ -7,6 +7,9 @@
 
 # Done on an external SSD 500Go Mac case sensitive partition.
 
+# https://source.android.com/setup/build/initializing#setting-a-file-descriptor-limit
+# Add "ulimit -S -n 1024" to "~/.bash_profile"
+
 # ############################################################################## #
 # ####                             Downloading                              #### #
 # ####           https://source.android.com/setup/build/downloading         #### #
@@ -71,3 +74,42 @@ m
 
 # ####                              Building                                #### #
 # ############################################################################## #
+
+# After the compulation, "echo $PATH" will give
+# /Volumes/Jonathan/aosp/prebuilts/jdk/jdk9/darwin-x86/bin:
+# /Volumes/Jonathan/aosp/out/soong/host/darwin-x86/bin:
+# /Volumes/Jonathan/aosp/out/host/darwin-x86/bin:
+# /Volumes/Jonathan/aosp/prebuilts/gcc/darwin-x86/x86/x86_64-linux-android-4.9/bin:
+# /Volumes/Jonathan/aosp/development/scripts:
+# /Volumes/Jonathan/aosp/prebuilts/devtools/tools:
+# /Volumes/Jonathan/aosp/external/selinux/prebuilts/bin:
+# /Volumes/Jonathan/aosp/prebuilts/android-emulator/darwin-x86_64:
+# /Volumes/Jonathan/aosp/prebuilts/asuite/acloud/darwin-x86:
+# /Volumes/Jonathan/aosp/prebuilts/asuite/aidegen/darwin-x86:
+# /Volumes/Jonathan/aosp/prebuilts/asuite/atest/darwin-x86:
+
+# ############################################################################## #
+# ####                                Run                                   #### #
+
+ANDROID_BUILD_OUT=/Volumes/Jonathan/aosp/out
+ANDROID_SDK_LINUX=/Users/jonathan/Library/Android/sdk
+ANDROID_BUILD=${ANDROID_BUILD_OUT}/target/product/generic
+
+${ANDROID_SDK_LINUX}/tools/emulator \
+    -sysdir ${ANDROID_BUILD} \
+    -system ${ANDROID_BUILD}/system.img \
+    -ramdisk ${ANDROID_BUILD}/ramdisk.img \
+    -data ${ANDROID_BUILD}/userdata.img \
+    -kernel ${ANDROID_SDK_LINUX}/system-images/android-18/armeabi-v7a/kernel-qemu \
+    -skindir ${ANDROID_SDK_LINUX}/platforms/android-18/skins \
+    -skin WVGA800 \
+    -scale 0.7 \
+    -memory 512 \
+    -partition-size 1024
+
+# ####                                Run                                   #### #
+# ############################################################################## #
+
+# Clean
+# https://source.android.com/setup/build/downloading#cleaning-up
+# make clobber
